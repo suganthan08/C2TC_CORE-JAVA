57acd5af8ff6f6b76cdc49af5cf72966b6d740e0
HP <HP@LAPTOP-BDJS79I4>
2024-10-25 23:32:42 +0530

    done

---
+++ C2TC_SUGANTHAN/src/com/tns/ifet/daytwo/IfDemo.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.daytwo;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			 
+			  int x = 5; 
+			  if (x != 5) 
+			   System.out.println("Value of x is not 5"); 
+
+		
+	}
+
+}
