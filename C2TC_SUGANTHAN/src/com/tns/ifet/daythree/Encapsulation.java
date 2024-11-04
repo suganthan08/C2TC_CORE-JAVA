3a2bf8d6f19987b701a9586b579dde1ef24814a9
HP <HP@LAPTOP-BDJS79I4>
2024-11-04 15:51:45 +0530

    java encapsulation demo

---
+++ C2TC_SUGANTHAN/src/com/tns/ifet/daythree/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.daythree;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Students obj = new Students(); 
+				obj.setSerialNum(101); 
+				obj.setName("Pooja"); 
+				obj.setAge(20); 
+				System.out.println(obj); 
+
+	}
+
+}
---
+++ C2TC_SUGANTHAN/src/com/tns/ifet/daythree/OopsConceptDemo.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.daythree;
+
+public class OopsConceptDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
